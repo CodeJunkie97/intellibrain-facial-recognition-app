a26e0dd2806af226bb328b2fc8e9af19cbb34824
CodeJunkie97 <ianmanners26@outlook.com>
2023-04-10 15:49:35 -0500

    Organizing Form and Button

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -17,7 +17,7 @@ const Logo = () => {
     return (
         <div className='ma4 mt0'>
             <Tilt className = "Tilt br2 shadow-2" options={defaultOptions}>
-                <div><img alt = 'logo' src = {einstein}/></div>
+                <div className = "Tilt-inner pa3"><img style = {{paddingTop:'30px'}}alt = 'logo' src = {einstein}/></div>
             </Tilt>
         </div>
     );
